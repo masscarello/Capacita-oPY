3655e3c471d5c2aedafb45a8e339d2a8c7ad442c
Guest <guest-0xroxo@lin>
2026-03-28 10:46:46 -0300

    ex 5 criado

---
+++ ex5.py
@@ -0,0 +1,15 @@
+#EX1
+Nascimento = int( input("Digite o ano de nascimento do usuario:"))
+AnoAtual = int(input("Digite em que ano estamos:"))
+
+Resposta = AnoAtual - Nascimento
+
+print(f"Sua idade e: {Resposta}")
+
+#EX2
+reais = int(input("Quantos reais deseja converter?"))
+dolar = float(input("Qual a cotacao do dolar?"))
+resposta2 = reais / dolar
+print(f"Convertendo seus reais em dolar...")
+print(f"{reais} resultam em {resposta2} dolares!")
+
