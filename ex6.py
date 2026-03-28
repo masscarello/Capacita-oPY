25c4885f2680034230b9833fea253e5f11903cd2
Guest <guest-0xroxo@lin>
2026-03-28 11:20:17 -0300

    exercicio 6 feito

---
+++ ex6.py
@@ -0,0 +1,20 @@
+Idade = int (input("Digite sua idade:"))
+
+if Idade < 18:
+    print("Menor de idade")
+elif Idade > 18 and Idade < 60:
+    print("Maior de idade")
+elif Idade >= 60:
+    print("idoso")
+
+
+nota1 = float(input("Digite a primeira nota do aluno:"))
+nota2 = float(input("Digite a segunda nota do aluno:"))
+nota3 = float(input("Digite a terceira nota do aluno:"))
+
+media = (nota1 + nota2 + nota3) / 3 
+
+if media >= 6.0:
+ print("Aprovado")
+else:
+   print("Reprovado")
